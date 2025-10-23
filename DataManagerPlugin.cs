ddc7c1e54bcc257f74085d0e2811e79cf614d43c
Diogo Pinela <diogoid7400@gmail.com>
2025-10-23 02:30:13 +0100

    Implement once-save data
    Once-save data is per-slot data that is set at the start of the game, and not
    changed thereafter.

--- DataManagerPlugin.cs
+++ DataManagerPlugin.cs
@@ -1,13 +1,167 @@
-using BepInEx;
+using Bep = BepInEx;
+using CG = System.Collections.Generic;
+using HL = HarmonyLib;
+using IO = System.IO;
+using Json = Newtonsoft.Json;
+using UE = UnityEngine;
 
 namespace Silksong.DataManager;
 
-[BepInAutoPlugin(id: "org.silksong-modding.datamanager")]
-public partial class DataManagerPlugin : BaseUnityPlugin
+[Bep.BepInAutoPlugin(id: "org.silksong-modding.datamanager")]
+public partial class DataManagerPlugin : Bep.BaseUnityPlugin
 {
-    private void Awake()
+    internal static DataManagerPlugin? Instance;
+
+    internal CG.List<(string GUID, IOnceSaveDataMod Mod)> onceSaveDataMods = new();
+
+    // We must use Start instead of Awake here - PluginInfos does not contain any mod instances
+    // when Awake runs.
+    private void Start()
     {
-        // Put your initialization logic here
-        Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+        Instance = this;
+        foreach (var (_, mod) in Bep.Bootstrap.Chainloader.PluginInfos)
+        {
+            if (mod.Instance is IOnceSaveDataMod modInstance)
+            {
+                Logger.LogInfo($"{mod.Metadata.GUID} uses once-save data");
+                onceSaveDataMods.Add((mod.Metadata.GUID, modInstance));
+            }
+        }
+        new HL.Harmony("org.silksong-modding.datamanager").PatchAll();
+    }
+
+    private static Json.JsonSerializerSettings jsonSettings = new()
+    {
+        TypeNameHandling = Json.TypeNameHandling.Auto,
+    };
+
+    private static string SaveDir(string subdir, int saveSlot)
+    {
+        // Other platforms are not relevant for modding.
+        var platform = (DesktopPlatform)Platform.Current;
+        return IO.Path.Combine(platform.saveDirPath, "Modded", subdir, saveSlot.ToString());
+    }
+
+    private const string OncePerSaveSubdir = "OncePerSave";
+    private const string SyncedFilenameSuffix = ".json.dat";
+
+    [HL.HarmonyPatch(
+        typeof(GameManager),
+        nameof(GameManager.SetLoadedGameData),
+        typeof(SaveGameData),
+        typeof(int)
+    )]
+    private static class OnceLoadHook
+    {
+        private static void Postfix(int saveSlot)
+        {
+            var mods = Instance!.onceSaveDataMods;
+
+            if (saveSlot == 0)
+            {
+                foreach (var (_, mod) in mods)
+                {
+                    mod.UntypedOnceSaveData = null;
+                }
+                return;
+            }
+
+            var saveDir = SaveDir(OncePerSaveSubdir, saveSlot);
+
+            foreach (var (guid, mod) in mods)
+            {
+                var saveFileName = IO.Path.Combine(saveDir, guid + SyncedFilenameSuffix);
+                try
+                {
+                    using var file = IO.File.OpenText(saveFileName);
+                    using var reader = new Json.JsonTextReader(file);
+                    var ser = Json.JsonSerializer.CreateDefault(jsonSettings);
+                    var obj = ser.Deserialize(reader, mod.OnceSaveDataType);
+                    mod.UntypedOnceSaveData = obj;
+                    Instance!.Logger.LogInfo($"Loaded save data for mod {guid}, slot {saveSlot}");
+                }
+                catch (IO.FileNotFoundException)
+                {
+                    mod.UntypedOnceSaveData = null;
+                }
+                catch (System.Exception err)
+                {
+                    mod.UntypedOnceSaveData = null;
+                    Instance!.Logger.LogError(
+                        $"Error loading save data for mod {guid}, slot {saveSlot}: {err}"
+                    );
+                }
+            }
+        }
+    }
+
+    [HL.HarmonyPatch(typeof(GameManager), nameof(GameManager.StartNewGame))]
+    private static class OnceSetupHook
+    {
+        private static void Postfix(GameManager __instance)
+        {
+            var saveSlot = __instance.profileID;
+            if (saveSlot == 0)
+            {
+                return;
+            }
+
+            var mods = Instance!.onceSaveDataMods;
+            var saveDir = SaveDir(OncePerSaveSubdir, saveSlot);
+
+            IO.Directory.CreateDirectory(saveDir);
+
+            foreach (var (guid, mod) in mods)
+            {
+                var data = mod.UntypedOnceSaveData;
+                if (data == null)
+                {
+                    continue;
+                }
+
+                var saveFileName = IO.Path.Combine(saveDir, guid + SyncedFilenameSuffix);
+                try
+                {
+                    using var file = IO.File.CreateText(saveFileName);
+                    using var writer = new Json.JsonTextWriter(file);
+                    var ser = Json.JsonSerializer.CreateDefault(jsonSettings);
+                    ser.Serialize(writer, data);
+                    Instance!.Logger.LogInfo($"Saved save data for mod {guid}, slot {saveSlot}");
+                }
+                catch (System.Exception err)
+                {
+                    Instance!.Logger.LogError(
+                        $"Error saving data for mod {guid}, slot {saveSlot}: {err}"
+                    );
+                }
+            }
+        }
+    }
+
+    [HL.HarmonyPatch(typeof(GameManager), nameof(GameManager.ClearSaveFile))]
+    private static class ClearHook
+    {
+        private static void Postfix(int saveSlot)
+        {
+            if (saveSlot == 0)
+            {
+                return;
+            }
+
+            var onceSaveDir = SaveDir(OncePerSaveSubdir, saveSlot);
+            try
+            {
+                IO.Directory.Delete(onceSaveDir, true);
+                Instance!.Logger.LogInfo($"Cleared modded data for slot {saveSlot}");
+            }
+            catch (IO.DirectoryNotFoundException)
+            {
+                Instance!.Logger.LogInfo($"No modded data to clear for slot {saveSlot}");
+            }
+            catch (System.Exception err)
+            {
+                Instance!.Logger.LogError($"Error clearing modded data for slot {saveSlot}: {err}");
+            }
+        }
     }
 }
---
+++ IOnceSaveDataMod.cs
@@ -0,0 +1,34 @@
+namespace Silksong.DataManager;
+
+/// Interface for mods that need to store some save data at the start of each new game,
+/// but don't need to change it afterwards.
+public interface IOnceSaveDataMod<T> : IOnceSaveDataMod
+    where T : class
+{
+    /// The once-save data for the current file.
+    /// SaveDataManager reads and stores its value once at the start of each new game,
+    /// after GameManager.StartNewGame completes.
+    /// If this property is null at that time, no data are written for that file for the client mod.
+    /// Upon loading any existing file, this property is set to the saved data for that file
+    /// for the client mod, if any exists.
+    /// If no such data exists, or the game is at the title screen, this property is set to null.
+    T? OnceSaveData { get; set; }
+
+    System.Type IOnceSaveDataMod.OnceSaveDataType => typeof(T);
+
+    object? IOnceSaveDataMod.UntypedOnceSaveData
+    {
+        get => OnceSaveData;
+        set => OnceSaveData = value == null ? null : (T)value;
+    }
+}
+
+/// An implementation detail that must be made public due to accessibility rules.
+/// Client mods should instead implement IOnceSaveDataMod<T>.
+public interface IOnceSaveDataMod
+{
+    /// The target type to use when deserializing save data for this mod.
+    System.Type OnceSaveDataType { get; }
+
+    object? UntypedOnceSaveData { get; set; }
+}
